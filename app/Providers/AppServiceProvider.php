404c0056bc5d8f01a55705ef74073b5710cb47e9
Rama Dhona Utama <ramadhona.utama@gmail.com>
2026-08-11 17:39:00 +0700

    Update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,6 +15,12 @@ class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
+        $helpers = app_path('helpers.php');
+
+        if (is_file($helpers)) {
+            require_once $helpers;
+        }
+
         $this->app->singleton(CmsPages::class);
     }
 
